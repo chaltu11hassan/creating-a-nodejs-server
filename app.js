e9dd41886c2312be67ea489108b310fea649d08b
Chaltu Hassan <chaltu11hassan@gmail.com>
2022-09-08 16:48:26 -0500

    routing requests

--- app.js
+++ app.js
@@ -13,8 +13,29 @@ const http = require("http");
 
 //or you can use an arrow function
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-//   process.exit(); //hard exit: quits the process
+  //   console.log(req.url, req.method, req.headers);
+  //   process.exit(); //hard exit: quits the process
+
+  //parse through url
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></input></form></body>"
+    );
+    res.write("</html>");
+    return res.end(); //return here so that it doesnt continue execution.
+  }
+
+  res.setHeader("Content-Type", "text/html");
+
+  res.write("<html>");
+  res.write("<head><title>My First Page</title><head>");
+  res.write("<body><h1>Hello from my Node.js server</h1></body>");
+  res.write("</html>");
+  res.end(); //can't write after this or else errors
 });
 
 //arguments: port # and host name
